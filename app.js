1141457b3532a47e4f775ea00d28eb864cd82458
William Condori <williamccondori@gmail.com>
2020-05-18 01:27:22 -0500

    Add zeroqm

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ videoCapture.set(cv.CAP_PROP_FRAME_HEIGHT, 300);
 
 const classifier = new cv.CascadeClassifier(cv.HAAR_FRONTALFACE_ALT2);
 
+const zmq = require("zeromq");
+const sockpy = zmq.socket("pair");
+sockpy.connect("tcp://127.0.0.1:4001");
+
 setInterval(() => {
   const frame = videoCapture.read();
 
@@ -29,19 +33,43 @@ setInterval(() => {
   );
 
   objects.forEach((object) => {
-    frame.drawRectangle(
-      new cv.Point(object.x, object.y),
-      new cv.Point(object.x + object.width, object.y + object.height),
-      {
-        color: new cv.Vec(0, 255, 0),
-        thickness: 1,
-      }
+    sockpy.send(
+      JSON.stringify({
+        type: "MASK",
+        frame: cv.imencode(".jpeg", frame).toString("base64"),
+      })
     );
+
+    sockpy.on("message", function (message) {
+      let result = message.toString();
+      const color = Boolean(result)
+        ? new cv.Vec(0, 255, 0)
+        : new cv.Vec(0, 0, 255);
+      const label = Boolean(result) ? "MASK" : "NO MASK";
+
+      console.log(label);
+      frame.drawRectangle(
+        new cv.Point(object.x, object.y),
+        new cv.Point(object.x + object.width, object.y + object.height),
+        {
+          color: color,
+          thickness: 1,
+        }
+      );
+      frame.putText(
+        label,
+        new cv.Point(object.x, object.y - 10),
+        2,
+        0.3,
+        color,
+        1
+      );
+    });
   });
 
   const image = cv.imencode(".jpeg", frame).toString("base64");
   io.emit("image", image);
-}, 1000 / FPS);
+}, 1000);
 
 http.listen(4000, () => {
   console.log("Listening on 4000");
